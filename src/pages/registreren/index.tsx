47ba225bf43ce7df7cf4e1f9105129e7dd7d9ff3
astronomx <unkownusernotfound@protonmail.com>
2023-03-10 12:43:45 +0100

    UPDATE: Verification is send to mailbox

--- src/pages/registreren/index.tsx
+++ src/pages/registreren/index.tsx
@@ -1,9 +1,11 @@
 import SignUp from "@/components/SignUp";
+import SignUpTest from "@/test/pages/SignUp.test"
 
 export default function Registreren() {
     return(
         <div>
             <SignUp />
+            {/* <SignUpTest /> */}
         </div>
 
     )
